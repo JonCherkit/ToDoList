9e35b041879ce44597a3c945c73d7b402760c754
Jonathan CHERKIT <j.cherkit@gmail.com>
2018-02-02 16:48:56 +0100

    Filtrage par état de tache disponible

--- todo.js
+++ todo.js
@@ -2,10 +2,10 @@
 
 const elementaryTask = class elementaryTask
 {
-	constructor(descritpion)
+	constructor(descritpion,status = false)
 	{
 		this._description = descritpion;
-		this._done = false;
+		this._done = status;
 	}
 
 	getDescription()
@@ -110,15 +110,58 @@ const toDo = class toDo
 
 }
 
+class toDoIterable extends toDo
+{
+	[Symbol.iterator]()
+	{
+		let index = -1
+		let data = this.getToDoList();
+
+		return{next: ()=>({value:data[++index],done:index<=data.length})};
+	}
+
+	getTaskByStatus(status)
+	{	
+		console.log(status);
+		//Ecrase l'affichage déjà présent
+		let display = document.querySelector('#toDoDisplay');
+		display.innerHTML = "";
+		//Créer un noeud <ul>
+		let node = document.createElement('UL');
+		for(let element of this._list)
+		{
+			if(element._done == status)
+			{
+				// créer un élément <li>
+				let li = document.createElement('LI');
+				// créer un noeud texte à partir de la description de la tache
+				let txt = document.createTextNode(element._description+' - Terminé ? : '+element._done);
+				// Ajoute le noeud texte au noeud li 
+				li.appendChild(txt);
+				// Ajoute le noeud txt+li au noeud ul
+				node.appendChild(li);
+			}
+		}
+		display.appendChild(node);
+		// console.log(node);
+	}
+}
+
 document.addEventListener('DOMContentLoaded',function(){
 	//Initialisation de la TodoList
-	let todo = new toDo();
+	let todo = new toDoIterable();
 
-	//Création d'une tache de base
+	//Création de taches
 	const task1 = new elementaryTask('Tondre la pelouse');
+	const task2 = new elementaryTask('Ramoner la cheminée',true);
+	const task3 = new elementaryTask('Prendre une pause (15mn)',true);
+	const task4 = new elementaryTask('épilier la tortue');
 
-	//Ajout de cette tache à la liste
+	//Ajout des taches à la liste
 	todo.addTask(task1);
+	todo.addTask(task2);
+	todo.addTask(task3);
+	todo.addTask(task4);
 
 	//Sélection de la zone d'affichage de la toDoList
 	let display = document.querySelector('#toDoDisplay');
@@ -131,9 +174,9 @@ document.addEventListener('DOMContentLoaded',function(){
 	//Trouve le noeud du bouton (#AddTaskButton) de la page Html
 	let button = document.querySelector('#AddTaskButton');
 
-	//Ecoute le noeud boutton
+	//Ecoute le noeud boutton 'Add'
 	button.addEventListener('click',function(){
-		//Recupere la valeur du champs texte
+		//Recupere le noeud du champs texte
 		let content = document.querySelector('#taskDescription');
 
 		//Créer une tache a partire de la valeur précedement recuperé
@@ -145,7 +188,15 @@ document.addEventListener('DOMContentLoaded',function(){
 		//Rafraichissement affichage
 		todo.refreshDisplay();
 
-		//Vide le champs de saisi de tache
+		//Vide le champs de saisie
 		content.value = "";
-	});	
+
+	});
+
+	//Ecoute le noeud boutton 'unAchieved'
+	document.querySelector('#unAchievedButton').addEventListener('click',() => todo.getTaskByStatus(false));
+	//Ecoute le noeud boutton 'achievedButton'
+	document.querySelector('#achievedButton').addEventListener('click',() => todo.getTaskByStatus(true));
+	//Ecoute le noeud boutton 'displayAllButton'
+	document.querySelector('#displayAllButton').addEventListener('click',() => todo.refreshDisplay());
 });
